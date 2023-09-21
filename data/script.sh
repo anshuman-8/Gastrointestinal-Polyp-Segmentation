eb4315fa5e3cab14987f4ac3cc32296e5aa48ba0
Anshuman <anshuman8swain@gmail.com>
2023-09-21 11:27:13 +0000

    basic train

---
+++ data/script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+wget https://datasets.simula.no/downloads/kvasir-seg.zip
+wget https://datasets.simula.no/downloads/kvasir-sessile.zip
+
+unzip kvasir-seg.zip
+unzip kvasir-sessile.zip
+
+rm kvasir-seg.zip
+rm kvasir-sessile.zip
+
+
+
